40e2d3b4cdd60cea840daae6eea4f8d7a24924b8
acsoliz <acsoliz@outlook.com>
2022-06-09 18:59:25 +0200

    controller box in main

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -60,60 +60,68 @@ const Main = () => {
   //------------------------------------------------
   return (
     <div>
-      <div>
-        <div>
-          <Searchbar />
-        </div>
-        <div>
-          <label>Ancestrys</label>
-          <select onChange={(e) => handleOnChangeAncestry(e)}>
-            <option value={"All"}>Todos</option>
-            <option value={"half-blood"}>half-blood</option>
-            <option value={"muggleborn"}>muggleborn</option>
-            <option value={"pure-blood"}>pure-blood</option>
-            <option value={"No tiene ancestro"}>No tiene ancestro</option>
-            <option value={"squib"}>squib</option>
-          </select>
-        </div>
+      <div className='tittle'>
+        <img src="" alt="" />
+        <h1>Harry Potter</h1>
+      </div>
+      <div className="container">
+        <aside className="side">
+          <h2>Controllers</h2>
+          <div>
+            <Searchbar />
+          </div>
+          <div>
+            <label>Ancestrys</label><br/>
+            <select onChange={(e) => handleOnChangeAncestry(e)}>
+              <option value={"All"}>Todos</option>
+              <option value={"half-blood"}>half-blood</option>
+              <option value={"muggleborn"}>muggleborn</option>
+              <option value={"pure-blood"}>pure-blood</option>
+              <option value={"No tiene ancestro"}>No tiene ancestro</option>
+              <option value={"squib"}>squib</option>
+            </select>
+          </div>
 
-        <div>
-          <label>House</label>
-          <select onChange={(e) => handleOnChangeHouses(e)}>
-            <option value={"All"}>Todos</option>
-            <option value={"Slytherin"}>Slytherin</option>
-            <option value={"Gryffindor"}>Gryffindor</option>
-            <option value={"Hufflepuff"}>Hufflepuff</option>
-            <option value={"Ravenclaw"}>Ravenclaw</option>
-            <option value={"Unknown"}>Unknown</option>
-          </select>
-        </div>
-        <div>
-          <div className="inputsContainer">
-            <div className="items">
-              <form className="nav-items">
-                <label>
-                  <input
-                    type="radio"
-                    checked={orderAlph.checked === "a-z"}
-                    onChange={handleChangeAlphabetic}
-                    value="a-z"
-                  />
-                  a-z
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    checked={orderAlph.checked === "z-a"}
-                    onChange={handleChangeAlphabetic}
-                    value="z-a"
-                  />
-                  z-a
-                </label>
-              </form>
+          <div>
+            <label>House</label><br/>
+            <select onChange={(e) => handleOnChangeHouses(e)}>
+              <option value={"All"}>Todos</option>
+              <option value={"Slytherin"}>Slytherin</option>
+              <option value={"Gryffindor"}>Gryffindor</option>
+              <option value={"Hufflepuff"}>Hufflepuff</option>
+              <option value={"Ravenclaw"}>Ravenclaw</option>
+              <option value={"Unknown"}>Unknown</option>
+            </select>
+          </div>
+          <div>
+            <div className="inputsContainer">
+              <div className="items">
+                <form className="nav-items">
+                  <label>
+                    <input
+                      type="radio"
+                      checked={orderAlph.checked === "a-z"}
+                      onChange={handleChangeAlphabetic}
+                      value="a-z"
+                    />
+                    a-z
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      checked={orderAlph.checked === "z-a"}
+                      onChange={handleChangeAlphabetic}
+                      value="z-a"
+                    />
+                    z-a
+                  </label>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
+        </aside>
+
+        <div className="content">
           <ul className="cards">
             {characters.length > 0 ? (
               characters.map((pj) => (
